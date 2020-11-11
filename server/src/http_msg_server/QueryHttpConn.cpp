4692be38050ab2fb26122496905c70283e952d9e
xiaominfc <xiaominfc@126.com>
2020-11-11 16:57:32 +0800

    review code

--- server/src/http_msg_server/QueryHttpConn.cpp
+++ server/src/http_msg_server/QueryHttpConn.cpp
@@ -19,10 +19,10 @@ QueryHttpConn::~QueryHttpConn(){
 
 void QueryHttpConn::HandleWork(){
 // 每次请求对应一个HTTP连接，所以读完数据后，不用在同一个连接里面准备读取下个请求
-	CHttpConn::HandleWork();
-	// char* in_buf = (char*)m_in_buf.GetBuffer();
-	// uint32_t buf_len = m_in_buf.GetWriteOffset();
-	// in_buf[buf_len] = '\0';
+	//CHttpConn::HandleWork();
+	char* in_buf = (char*)m_in_buf.GetBuffer();
+	uint32_t buf_len = m_in_buf.GetWriteOffset();
+	in_buf[buf_len] = '\0';
 
 	//log("OnRead, buf_len=%u, conn_handle=%u\n", buf_len, m_conn_handle); // for debug
 
